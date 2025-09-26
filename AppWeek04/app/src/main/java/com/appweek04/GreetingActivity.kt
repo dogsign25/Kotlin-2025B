5181604b121650f1ef7ceafe13cd2e32c3619cb4
Kim <djkim083125@gmail.com>
2025-09-26 16:01:16 +0900

    week04 : Greeting App v0.3

---
+++ AppWeek04/app/src/main/java/com/appweek04/GreetingActivity.kt
@@ -0,0 +1,33 @@
+package com.appweek04
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class GreetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greeting)
+        val editTextName = findViewById<EditText>(R.id.editTextName)
+        val buttonGreet = findViewById<Button>(R.id.buttonGreet)
+        val textviewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+
+        var greeting: String = ""
+        buttonGreet.setOnClickListener{
+            val name = editTextName.text.toString().trim()
+
+            if(name.isNotEmpty()){
+                greeting = "안녕, ${name}님~"
+            } else {
+                greeting = "너의 이름은?"
+            }
+            textviewGreeting.text = greeting
+            textviewGreeting.visibility = View.VISIBLE
+            Log.d("KotlinWeek04App", greeting)
+        }
+    }
+}
