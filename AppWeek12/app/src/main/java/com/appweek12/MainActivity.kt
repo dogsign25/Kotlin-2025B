0cc11e83506069ae762871d958de8ba17a2c13d9
Kim <djkim083125@gmail.com>
2025-11-28 16:17:04 +0900

    week12 : Counter App v0.6(MVVM : StateFlow)

--- AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
+++ AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
@@ -4,7 +4,11 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.appweek12.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,15 +24,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupObservers() {
-        viewModel.count.observe(this){
-                count -> binding.textViewCount.text = count.toString()
-
-            when{
-                count > 0 -> binding.textViewCount.setTextColor(Color.BLUE)
-                count < 0 -> binding.textViewCount.setTextColor(Color.RED)
-                else -> binding.textViewCount.setTextColor(Color.BLACK)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.count.collect{
+                    count -> binding.textViewCount.text = count.toString()
+                    when{
+                        count > 0 -> binding.textViewCount.setTextColor(Color.BLUE)
+                        count < 0 -> binding.textViewCount.setTextColor(Color.RED)
+                        else -> binding.textViewCount.setTextColor(Color.BLACK)
+                    }
+                }
             }
         }
+
     }
 
     private fun setupListeners() {
